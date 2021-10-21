111c61788a14d8f853c887692c7d6c0359a17301
Alexandra-Sontu <71020688+Alexandra-Sontu@users.noreply.github.com>
2021-10-21 16:13:24 +0300

    Update problema 7.py

--- problema 7.py
+++ problema 7.py
@@ -1,31 +1,35 @@
 s=str(input("Dati un sir:"))
 a=s.count("A")
 print("Numarul de aparitii ale caracterului 'A' in sir:",a)
-print(a)
 if "A" in s:
-for i in s:
+ for i in s:
     x=s.replace("A","*")
+    print("Sirul obtinut in urma substituirii:",x)
 else:
-    print(x)
+    print("x)",s)
 if "B" in s:
-for i in s:
+ for i in s:
     b=s.replace("B","")
- else:
-    print("Sirul obtinut prin radierea a tuturor aparitiilor caracterului 'B' in sir:",b)
- if "MA" in s:
-  for i in s:
-c=s.count("MA")
+    print("Sirul obtinut prin radierea a tuturor aparitiilor caracterului 'B': ",b)
 else:
-print("Numarul de aparitii ale silabei'MA' in sir:",c)
+    print("b)",s)
+if "MA" in s:
+  for i in s:
+    c=s.count("MA")
+    print("Numarul de aparitii a silabei 'MA': ",c)
+else:
+    print("c) Nu avem silaba 'MA' in sirul dat")
 if "MA"  in s:
   for i in s:
     y=s.replace("MA","TA")
+    print("Substituirea silabei 'MA' cu 'TA': ",y)
+if "TO" in s:
+  for i in s:   
+    z=s.replace("TO","")
+    print(" Sirul obtinut prin radierea a tuturor aparitiilor silabe 'TO': ",z)
 else:
-    print(y)
-for i in s:
-    d=s.replace("TO","")
-    print("Sirul obtinut prin radierea a tuturor aparitiilor caracterului 'TO' in sir:",d)
-    z=len(s)
-    e=s[s::-1]
-    print("Sirul invers:",e)
+    print("z) ",s)
+d=len(s)
+g=s[d::-1]
+print("d) Sirul invers:",d)
 
